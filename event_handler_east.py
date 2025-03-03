b5173d97efe8a69e596d3ac9f7f12f40541bc9db
ankitjain3288 <49764211+ankitjain3288@users.noreply.github.com>
2025-03-03 00:23:47 -0500

    Update event_handler_east.py

--- event_handler_east.py
+++ event_handler_east.py
@@ -12,31 +12,44 @@ remote_region = "west"
 dynamodb_local = boto3.resource("dynamodb")
 table_local = dynamodb_local.Table(table_name)
 
-def update_handler(item):
-    retVal = false
-    try:
-        table.update_item(
-                Key={"event_id": event_id},
-                UpdateExpression="SET #st = :st, version = :new_version",
-                ConditionExpression="version = :expected_version",
-                ExpressionAttributeNames={"#st": "status"},
-                ExpressionAttributeValues={
-                    ":st": updated_status,
-                    ":new_version": updated_version,
-                    ":expected_version": current_version
-                }
-            )
-         retVal = True
-        
-    except ClientError as e:
-                # Check if it's a conditional check failure (version mismatch)
-                if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
-                    retVal = false
-    return retVal
-                    
-                
-    
+def CreateNewEvent()
+        read_success = stale_read_handler(id)
+    if read_success:
+        # Prepare the new version and updated data
+        updated_version = current_version + 1
+        updated_status = "processed"
 
+        max_retries = 3
+        success = False
+        attempts = 0
+        
+        while attempts < max_retries:
+            success = update_handler(updated_status,updated_version,current_version)
+            if(success)
+               new_event_id = str(uuid.uuid4())
+                new_item = {
+                    "event_id": new_event_id,
+                    "event_type": "EventB",
+                    "source_event_id": event_id,
+                    "version": 0,          # Start at version 0 for the new item
+                    "status": "new"
+                }
+                table.put_item(Item=new_item)
+                break;
+            # Re-read the item to get the latest version
+            latest_item = table.get_item(Key={"event_id": event_id}).get("Item", {})
+            latest_version = latest_item.get("version", 0)
+
+            # Prepare to retry
+            current_version = latest_version
+            updated_version = current_version + 1
+            attempts += 1
+            time.sleep(1)  # simple backoff
+        return
+
+def handleReplicationRecordEvent(item)
+    
+                    
 def read_remote_region(id):
     dynamodb_remote = boto3.resource('dynamodb', region_name=remote_region)
     table_remote = dynamodb_remote.Table(table_name)
@@ -77,45 +90,9 @@ def handleLocalUpdateForEvent(id,region):
                     # Check if it's a conditional check failure (version mismatch)
                     if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
                         retVal = False
-    else
         
     return retVal
 
-
-
-    
-    read_success = stale_read_handler(id)
-    if read_success:
-        # Prepare the new version and updated data
-        updated_version = current_version + 1
-        updated_status = "processed"
-
-        max_retries = 3
-        success = False
-        attempts = 0
-        
-        while attempts < max_retries:
-            success = update_handler(updated_status,updated_version,current_version)
-            if(success)
-               new_event_id = str(uuid.uuid4())
-                new_item = {
-                    "event_id": new_event_id,
-                    "event_type": "EventB",
-                    "source_event_id": event_id,
-                    "version": 0,          # Start at version 0 for the new item
-                    "status": "new"
-                }
-                table.put_item(Item=new_item)
-                break;
-            # Re-read the item to get the latest version
-            latest_item = table.get_item(Key={"event_id": event_id}).get("Item", {})
-            latest_version = latest_item.get("version", 0)
-
-            # Prepare to retry
-            current_version = latest_version
-            updated_version = current_version + 1
-            attempts += 1
-            time.sleep(1)  # simple backoff
 
 
 def lambda_handler(event, context):
@@ -129,8 +106,8 @@ def lambda_handler(event, context):
         #current_version = new_image.get("version", {}).get("N")
         region = new_image.get("region", {}).get("S")
         if region == local_region and event_type == "EventA":
-            handleLocalUpdateForEvent(id,region)
+            handleLocalRecordEvent(id,region)
         else
-            handleReplicationUpdate(id)
+            handleReplicationRecordEvent(new_image)
 
     return {"statusCode": 200, "body": json.dumps("Stream processing complete.")}
