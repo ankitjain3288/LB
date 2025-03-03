0d07819e61b1073c669d5bd98df5daca89af1d05
ankitjain3288 <49764211+ankitjain3288@users.noreply.github.com>
2025-03-02 22:49:17 -0500

    Update event_handler_east.py

--- event_handler_east.py
+++ event_handler_east.py
@@ -9,10 +9,10 @@ table_name="NetworkEvents"
 local_region = "east"
 remote_region = "west"
 
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(TABLE_NAME)
+dynamodb_local = boto3.resource("dynamodb")
+table_local = dynamodb_local.Table(table_name)
 
-def update_handler(updated_status,updated_version,current_version):
+def update_handler(item):
     retVal = false
     try:
         table.update_item(
@@ -37,24 +37,55 @@ def update_handler(updated_status,updated_version,current_version):
                 
     
 
-def read_region(region,key):
-
-    dynamodb = boto3.resource('dynamodb', region_name=region)
-    table = dynamodb.Table(table_name)
-    response = table.get_item(Key=key, ConsistentRead=True)
+def read_remote_region(id):
+    dynamodb_remote = boto3.resource('dynamodb', region_name=remote_region)
+    table_remote = dynamodb_remote.Table(table_name)
+    response = table.get_item(Key=id, ConsistentRead=True)
     return response.get('Item')
 
-def stale_read_handler(key):
+def stale_read_handler(id):
     retVal = false
     item_from_local_read = read_region(local_region,key)
-    item_from_external_read = read_region(remote_region,key)
-    if item_from_local_read.get('version', 0) > item_from_external_read.get('version', 0)
+    item_from_remoteregion_read = read_region(remote_region,key)
+    if item_from_local_read.get('version') > item_from_remoteregion_read.get('version')
        retVal = true
     return retVal
 
-def handlelocalUpdate():
-    read_success = stale_read_handler(event_id)
-    if(read_success):
+def handleLocalUpdateForEvent(id,region):
+    item_local = table_local.get_item(Key=id, ConsistentRead=True)
+    item_remote = read_remote_region(id)
+    retVal = False
+    if item_local.get('version') > item_remote.get('version')
+        current_version = item.get('version')
+        updated_version = current_version + 1
+        updated_status = "processed"
+        try:
+            table_local.update_item(
+                    Key={"id": id},
+                    UpdateExpression="SET #st = :st, version = :new_version",
+                    ConditionExpression="version = :expected_version",
+                    ExpressionAttributeNames={"#st": "status"},
+                    ExpressionAttributeValues={
+                        ":st": updated_status,
+                        ":new_version": updated_version,
+                        ":expected_version": current_version
+                    }
+                )
+             retVal = True
+            
+        except ClientError as e:
+                    # Check if it's a conditional check failure (version mismatch)
+                    if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+                        retVal = False
+    else
+        
+    return retVal
+
+
+
+    
+    read_success = stale_read_handler(id)
+    if read_success:
         # Prepare the new version and updated data
         updated_version = current_version + 1
         updated_status = "processed"
@@ -88,18 +119,18 @@ def handlelocalUpdate():
 
 
 def lambda_handler(event, context):
-    
+    # we will have each items having version , region as attributes in global table
     for record in event.get("Records", []):
         
         new_image = record["dynamodb"].get("NewImage", {})
-        event_id = new_image.get("event_id", {}).get("S")
+        id = new_image.get("id", {}).get("S")
         event_type = new_image.get("event_type", {}).get("S")
         
-        current_version = new_image.get("version", {}).get("N")
-        originating_region = new_image.get("region", {}).get("S")
-        if event_type == "eventA" and originating_region == "local_region":
-            handlelocalUpdate(event_id)
+        #current_version = new_image.get("version", {}).get("N")
+        region = new_image.get("region", {}).get("S")
+        if region == local_region and event_type == "EventA":
+            handleLocalUpdateForEvent(id,region)
         else
-            handleReplicationUpdate()
+            handleReplicationUpdate(id)
 
     return {"statusCode": 200, "body": json.dumps("Stream processing complete.")}
